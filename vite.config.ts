70baabe4b9a565578cbd1ed9e06aaa5087fd39cd
26im0079-a11y <26im0079@i-u.ac.jp>
2026-06-24 01:04:05 +0000

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-vite-app/', // 👈 この行を新しく追加してください！
   plugins: [react(), tailwindcss()],
 })
